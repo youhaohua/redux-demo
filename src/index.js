788e4f1d4a0f2f56eaf0237746fc3afa04cf8366
游昊华 <youhaohua@gosuncn.com>
2018-09-05 15:47:14 +0800

    调试配置

--- src/index.js
+++ src/index.js
@@ -1,55 +1,53 @@
- import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore,applyMiddleware} from "redux"
+import {createStore, applyMiddleware, compose} from "redux"
 import thunk from "redux-thunk"
-import {count,add_gun,remove_gun,addAsync} from "./index.redux"
-const store=createStore(count,applyMiddleware(thunk))
-function render(){
-    ReactDOM.render(<App store={store} add_gun={add_gun} remove_gun={remove_gun} addAsync={addAsync}  />, document.getElementById('root'));
+import {count, add_gun, remove_gun, addAsync} from "./index.redux"
+const store = createStore(count, compose(applyMiddleware(thunk),window.devToolsExtension?window.devToolsExtension():f=>f ))
+function render() {
+    ReactDOM.render(
+        <App
+        store={store}
+        add_gun={add_gun}
+        remove_gun={remove_gun}
+        addAsync={addAsync}/>, document.getElementById('root'));
     registerServiceWorker();
- }
+}
 render()
 store.subscribe(render)
-
- 
-
-
-
-
-
 
 /* import {createStore} from "redux"
 const initstate={
     time:0
-  
+
   }
 function count(state=initstate,action){
-  
+
   switch(action.type){
 
    case "加机关枪":
    return {...state,time:state.time+1 }
-  
+
    case "减机关枪":
 
    return {...state,time:state.time-1 }
-   
+
    default: return {...state,time:0}
 
-  }  
+  }
  }
  const store=createStore(count)
  store.subscribe(listen)
 
  function listen(){
-   
+
     const mystate=store.getState()
-    
+
     console.log(mystate)
- 
+
   }
 
 store.dispatch({type:"加机关枪"})
